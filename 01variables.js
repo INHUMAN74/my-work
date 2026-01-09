d1d5686c19d47ad74624f7ae61bd0163b4adca10
SH4NU <shanuyadav7474@gmail.com>
2026-01-10 00:18:01 +0530

    2nd one

---
+++ 01variables.js
@@ -0,0 +1,7 @@
+//second word first letter capital
+const accountID=1000     // cant be changed
+let accountEMail="sumit@google.com"     //changebale
+var accountPassword="1234"
+accountCity= "Delhi"
+
+console.table([accountID, accountEMail,accountPassword,accountCity])
---
+++ 02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict";//treat all js code as newer version
+
+//alert(3+3) //we are using nodejs,not browser so it wont run
+
+//code readability should be high
+
+let name="sumit"
+let age=33
+
+//number=> 2 to power 53
+//bigint 
+//string
+//boolean=>true/false
+//null=>standalone value not considered as 0
+//undefined=> value not assigned yet
+//symbol=> to ensure uniqueness
+
+console.log(typeof null)
